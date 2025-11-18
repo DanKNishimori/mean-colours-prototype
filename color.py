c9e66f2fac6dd010ae859e486620adc7bc782f86
Daniel Nishimori <dankazukinishimori@gmail.com>
2025-11-18 11:43:06 -0300

    refactor: add Byte class for RGB channels in Color
    Since integers doesn't have a limit and can be negative, some issues
    could happen, using a Byte class with 8 bits size ensures the values is limited from 0 to 255.

--- color.py
+++ color.py
@@ -1,4 +1,7 @@
 from random import uniform
+from typing import Union
+
+Bytes = list["Byte"]
 
 
 def clip(base: int | float, min_v: int | float, max_v: int | float) -> int | float:
@@ -7,11 +10,6 @@ def clip(base: int | float, min_v: int | float, max_v: int | float) -> int | flo
     elif base > max_v:
         return max_v
     return base
-
-
-def to_byte(hex_num: int) -> str:
-    num = hex(hex_num).replace("0x", "")
-    return "0" + num if len(num) < 2 else num
 
 
 def fill_sequence(n0: int | float, nf: int | float, l: int) -> list[int]:
@@ -27,20 +25,43 @@ def random_deviation(
     return uniform(min_v, max_v)
 
 
+class Byte:
+    def __init__(self, value: int):
+        self.value = int(clip(value, 0, 255))
+
+    def from_str(value: str) -> "Byte":
+        return Byte(int(value, 16))
+
+    def as_int(self) -> int:
+        return self.value
+
+    def __repr__(self) -> str:
+        num = hex(self.value).replace("0x", "")
+        return "0" + num if len(num) < 2 else num
+
+    def __add__(self, target: Union["Byte", int]) -> "Byte":
+        value: int = target.value if type(target) is Byte else target
+        return Byte(self.value + value)
+
+    def __sub__(self, target: Union["Byte", int]) -> "Byte":
+        value: int = target.value if type(target) is Byte else target
+        return Byte(self.value - value)
+
+
 class Color:
     def __init__(self, color: str | tuple[int, int, int]):
         if type(color) is tuple:
-            self.red = int(color[0])
-            self.green = int(color[1])
-            self.blue = int(color[2])
+            self.red = Byte(color[0])
+            self.green = Byte(color[1])
+            self.blue = Byte(color[2])
         else:
-            color_code = color.replace("#", "")
-            self.red = int(color_code[0:2], 16)
-            self.green = int(color_code[2:4], 16)
-            self.blue = int(color_code[4:6], 16)
+            color_code = str(color).replace("#", "")
+            self.red = Byte.from_str(color_code[0:2])
+            self.green = Byte.from_str(color_code[2:4])
+            self.blue = Byte.from_str(color_code[4:6])
 
     @staticmethod
-    def zip_rgb(*args: "Color") -> tuple[list[int], list[int], list[int]]:
+    def zip_rgb(*args: "Color") -> tuple[Bytes, Bytes, Bytes]:
         r, g, b = [], [], []
         for i in args:
             r.append(i.red)
@@ -53,19 +74,18 @@ class Color:
         red, green, blue = 0, 0, 0
         length = len(colors)
         for c in colors:
-            red += c.red
-            green += c.green
-            blue += c.blue
+            red += c.red.as_int()
+            green += c.green.as_int()
+            blue += c.blue.as_int()
         return Color((red / length, green / length, blue / length))
 
     @staticmethod
     def invert(color: "Color") -> "Color":
-        base = Color("#FFFFFF")
         return Color(
             (
-                abs(color.red - base.red),
-                abs(color.green - base.green),
-                abs(color.blue - base.blue),
+                (Byte(255) - color.red).as_int(),
+                (Byte(255) - color.green).as_int(),
+                (Byte(255) - color.blue).as_int(),
             )
         )
 
@@ -74,18 +94,20 @@ class Color:
         first_color: "Color", last_color: "Color", length: int
     ) -> list["Color"]:
         (red, green, blue) = Color.zip_rgb(first_color, last_color)
-        red = fill_sequence(red[0], red[1], length)
-        green = fill_sequence(green[0], green[1], length)
-        blue = fill_sequence(blue[0], blue[1], length)
+        red = fill_sequence(red[0].as_int(), red[1].as_int(), length)
+        green = fill_sequence(green[0].as_int(), green[1].as_int(), length)
+        blue = fill_sequence(blue[0].as_int(), blue[1].as_int(), length)
         return [Color(rgb) for rgb in zip(red, green, blue)]
 
     @staticmethod
     def derive_randomly(base_color: "Color", dev_level: float) -> "Color":
         [max_range, min_range] = [(5, 255), (0, 220)]
         [red, green, blue] = [
-            random_deviation(base_color.red, dev_level, min_range, max_range),
-            random_deviation(base_color.green, dev_level, min_range, max_range),
-            random_deviation(base_color.blue, dev_level, min_range, max_range),
+            random_deviation(base_color.red.as_int(), dev_level, min_range, max_range),
+            random_deviation(
+                base_color.green.as_int(), dev_level, min_range, max_range
+            ),
+            random_deviation(base_color.blue.as_int(), dev_level, min_range, max_range),
         ]
         return Color((red, green, blue))
 
@@ -93,7 +115,7 @@ class Color:
         return f"Color<R={self.red};G={self.green};B={self.blue}>"
 
     def __str__(self) -> str:
-        return f"#{to_byte(self.red)}{to_byte(self.green)}{to_byte(self.blue)}"
+        return f"#{self.red}{self.green}{self.blue}"
 
     def __add__(self, value: "Color") -> "Color":
         return Color(
